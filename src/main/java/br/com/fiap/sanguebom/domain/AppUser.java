204e3e7e0376eba6228f351e0f03b2fbfdde6fff
rodfc773 <rodrigo.fc773@gmail.com>
2026-08-24 22:36:56 -0300

    Fix: Corrigindo erros de inicação e fazendo o flyway funcionar

--- src/main/java/br/com/fiap/sanguebom/domain/AppUser.java
+++ src/main/java/br/com/fiap/sanguebom/domain/AppUser.java
@@ -21,7 +21,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 
 @Entity
-@Table(name = "AppUsers")
+@Table(name = "app_user")
 @EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
--- src/main/java/br/com/fiap/sanguebom/domain/Notification.java
+++ src/main/java/br/com/fiap/sanguebom/domain/Notification.java
@@ -20,7 +20,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 
 @Entity
-@Table(name = "Notifications")
+@Table(name = "notification")
 @EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
