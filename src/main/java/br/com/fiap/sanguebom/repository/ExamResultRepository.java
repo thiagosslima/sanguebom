082e62b030e9d51547bc5e439011829a21482e92
Igor Costa <icgsbr@gmail.com>
2026-08-29 15:52:50 -0300

    feat: consultas de exame e perfil por titular

--- src/main/java/br/com/fiap/sanguebom/repository/ExamResultRepository.java
+++ src/main/java/br/com/fiap/sanguebom/repository/ExamResultRepository.java
@@ -2,8 +2,16 @@ package br.com.fiap.sanguebom.repository;
 
 import br.com.fiap.sanguebom.model.entities.ExamResult;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ExamResultRepository extends JpaRepository<ExamResult, Long> {
+
+    @Query("select r from ExamResult r join fetch r.examItem item "
+            + "where r.exam.id = :examId order by item.name")
+    List<ExamResult> findByExamIdWithItem(@Param("examId") Long examId);
 }
